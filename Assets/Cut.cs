2396a0375dcc4e5770aaa6bbc7f2be1f1d886ef7
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-10-08 22:56:38 +0900

    初

---
+++ Assets/Cut.cs
@@ -0,0 +1,19 @@
+using BLINDED_AM_ME;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cut : MonoBehaviour
+{
+    [SerializeField] GameObject _cutObject;
+    [SerializeField] Transform _anchorPoint;
+    [SerializeField] Transform _normalDirection;
+    [SerializeField] Material _cutMesh; 
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            MeshCut.Cut(_cutObject, _anchorPoint.position, _normalDirection.right, _cutMesh);
+        }
+    }
+}
